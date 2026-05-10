5168ca06bc0bad70c024a43a50a81a684adccb56
Ash21-bussiness <aishah.wasim2004@gmail.com>
2026-05-10 18:32:47 +0530

    Add files via upload

---
+++ lesson 1/activity1.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+print("My name is Alex")
+print("i love playing soccer.")
+print("the result of 3 + 5 is:", 3+5)
+bag= "Happy Birthday"
+print (bag)
+color= input("Enter your fav colour?")
+print(color)
