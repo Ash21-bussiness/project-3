df9e7ed101723cc47cd4b76136b0190b942a26bd
Ash21-bussiness <aishah.wasim2004@gmail.com>
2026-05-17 18:58:57 +0530

    Add files via upload

---
+++ lesson 3/activity4.py
@@ -0,0 +1,7 @@
+import datetime
+current_time = datetime.datetime.now()
+
+print("Time now at greenwich merdian is:", end ="")
+print(current_time)
+import calendar
+print("\n", calendar.calendar(2021))
